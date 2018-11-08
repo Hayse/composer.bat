f30a22ddd47588be59f6213f1764f0319a527dca
Hayse <haysinfo@163.com>
2018-11-08 13:15:52 +0800

    Update composer.install.bat

--- composer.install.bat
+++ composer.install.bat
@@ -3,28 +3,40 @@
 echo Composer install ...
 
 IF NOT EXIST php.exe goto err1
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+IF NOT EXIST composer-setup.php goto err2
 
-php -r "readfile('https://getcomposer.org/installer');" | php
-IF NOT EXIST composer.phar goto err2
+php composer-setup.php
+IF NOT EXIST composer.phar goto err3
 
 echo @php "%~dp0composer.phar" %*>composer.bat
-IF NOT EXIST composer.bat goto err3
+IF NOT EXIST composer.bat goto err4
 
-echo composer is installer .
-rem composer -V
-rem the composer installer  OK
+echo Composer installation is successful !
+composer -V
+
+php -r "unlink('composer-setup.php');"
+IF EXIST composer-setup.php goto err5
 goto ends
 
 :err1
-echo Error : file not found with php.exe
+echo Error : File not found with php.exe
 goto ends
 
 :err2
-echo Error : file not download with composer.phar
+echo Error : File not download with composer-setup.php
 goto ends
 
 :err3
-echo Error : file not created with composer.bat
+echo Error : File not created with composer.phar
+goto ends
+
+:err4
+echo Error : File not created with composer.bat
+goto ends
+
+:err5
+echo Warning : Install the temporary file is not clear with composer-setup.php
 goto ends
 
 :ends
