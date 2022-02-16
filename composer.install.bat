3a505fbc68edc1cfb39d9c650602eb5a699be2cc
Hayse <haysinfo@163.com>
2022-02-16 10:10:27 +0800

    Update composer.install.bat

--- composer.install.bat
+++ composer.install.bat
@@ -1,8 +1,9 @@
-::_installer composer ,, download composer.phar ,, and composer.bat
+::_installer composer ,, download composer.phar ,, and create composer.bat
 @echo off
 echo Composer install ...
 
 IF NOT EXIST php.exe goto err1
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 IF NOT EXIST composer-setup.php goto err2
 
@@ -12,33 +13,33 @@ IF NOT EXIST composer.phar goto err3
 echo @php "%~dp0composer.phar" %*>composer.bat
 IF NOT EXIST composer.bat goto err4
 
+del composer-setup.php
+IF EXIST composer-setup.php goto err5
+
 echo Composer installation is successful !
 composer -V
 
-php -r "unlink('composer-setup.php');"
-IF EXIST composer-setup.php goto err5
 goto ends
 
 :err1
-echo Error : File not found with php.exe
+echo Error : File not found : php.exe
 goto ends
 
 :err2
-echo Error : File not download with composer-setup.php
+echo Error : File not download : composer-setup.php
 goto ends
 
 :err3
-echo Error : File not created with composer.phar
+echo Error : File not created : composer.phar
 goto ends
 
 :err4
-echo Error : File not created with composer.bat
+echo Error : File not created : composer.bat
 goto ends
 
 :err5
-echo Warning : Install the temporary file is not clear with composer-setup.php
+echo Warning : Install the temporary file is not clear : composer-setup.php
 goto ends
 
 :ends
-pause
 exit
